6a1648b81a9d5a6aed0554be3811106aeedcd6cb
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-05 00:01:35 +0800

    page(tabs): add loading widget

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,15 @@
 import React from "react";
 import { View } from "react-native";
 
+import { useUser } from "@/providers/UserProvider";
+
+import LoadingWidget from "@/components/Widgets/LoadingWidget";
+
 export default function HomeScreen() {
+  const { isLoading } = useUser();
+
+  if (isLoading) return <LoadingWidget showLogo={false} />;
+
   return (
     <View
       style={{
