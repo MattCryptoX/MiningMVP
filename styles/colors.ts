0b4f90537c8707092180765a6c0a1a39634417c3
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 20:37:21 +0800

    feat(styles): init colors

---
+++ styles/colors.ts
@@ -0,0 +1,25 @@
+// styles/colors.ts
+export const colors = {
+  light: {
+    view: {
+      primary: "#25242B",
+      secondary: "#17171B",
+      tertiary: "#35343B",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+    accent: "#20AB7D",
+  },
+  dark: {
+    view: {
+      primary: "#25242B",
+      secondary: "#17171B",
+      tertiary: "#35343B",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+    accent: "#20AB7D",
+  },
+};
