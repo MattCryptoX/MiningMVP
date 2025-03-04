2c93b722f9685f14da0e1a354f2652e956be135a
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 23:09:21 +0800

    feat(convex): fix domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://relieved-eagle-60.clerk.accounts.dev/",
+      domain: "https://relieved-eagle-60.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
