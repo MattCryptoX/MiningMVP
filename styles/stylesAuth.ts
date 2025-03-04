16d5b55bdb530478352aa18fc312fbcd766861bf
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 23:37:34 +0800

    feat(styles): auth & widget styles

---
+++ styles/stylesAuth.ts
@@ -0,0 +1,35 @@
+// styles/Auth.ts
+import { fonts } from "@/styles/fonts";
+import { colors } from "@/styles/colors";
+
+import { Dimensions, StyleSheet } from "react-native";
+
+import { Theme } from "@/types/settings";
+
+const { width } = Dimensions.get("window");
+
+export const createAuth = (theme: Theme) => {
+  const themeColors = theme === "Dark" ? colors.dark : colors.light;
+
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: themeColors.view.primary,
+    },
+    screen: {
+      flex: 1,
+      justifyContent: "space-evenly",
+      alignItems: "center",
+    },
+    textWhite: {
+      fontSize: 15,
+      color: themeColors.white,
+      fontFamily: fonts.inter.medium,
+    },
+    textPrimary: {
+      fontSize: 15,
+      color: themeColors.accent,
+      fontFamily: fonts.inter.bold,
+    },
+  });
+};
---
+++ styles/stylesWidget.ts
@@ -0,0 +1,107 @@
+// styles/Widget.ts
+import { fonts } from "@/styles/fonts";
+import { colors } from "@/styles/colors";
+
+import { StyleSheet } from "react-native";
+
+import { Theme } from "@/types/settings";
+export const createLoading = (theme: Theme) => {
+  const themeColors = theme === "Dark" ? colors.dark : colors.light;
+
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: themeColors.view.primary,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+};
+
+export const createInput = (theme: Theme) => {
+  const themeColors = theme === "Dark" ? colors.dark : colors.light;
+
+  return StyleSheet.create({
+    label: {
+      fontSize: 15,
+      color: themeColors.accent,
+      fontFamily: fonts.inter.bold,
+    },
+    asterisk: {
+      fontSize: 15,
+      color: themeColors.negative,
+      fontFamily: fonts.inter.bold,
+    },
+    view: {
+      height: 60,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      paddingHorizontal: 20,
+      borderRadius: 10,
+    },
+    viewPrimary: {
+      backgroundColor: themeColors.view.primary,
+    },
+    viewSecondary: {
+      backgroundColor: themeColors.view.secondary,
+    },
+    input: {
+      fontSize: 15,
+      color: themeColors.text.primary,
+      fontFamily: fonts.inter.semiBold,
+    },
+    pinCodeContainer: {
+      width: 50,
+      height: 50,
+      borderRadius: 10,
+      borderColor: themeColors.view.primary,
+      backgroundColor: themeColors.view.secondary,
+    },
+    pinCodeText: {
+      fontSize: 15,
+      color: themeColors.text.primary,
+      fontFamily: fonts.inter.semiBold,
+    },
+    placeholderText: {
+      marginBottom: 5,
+    },
+  });
+};
+
+export const createButton = (theme: Theme) => {
+  const themeColors = theme === "Dark" ? colors.dark : colors.light;
+
+  return StyleSheet.create({
+    button: {
+      gap: 5,
+      height: 60,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 10,
+    },
+    viewDisabled: {
+      backgroundColor: themeColors.view.disabled,
+    },
+    viewPrimary: {
+      backgroundColor: themeColors.accent,
+    },
+    viewSecondary: {
+      backgroundColor: themeColors.view.secondary,
+    },
+    text: {
+      fontSize: 17.5,
+      fontFamily: fonts.inter.bold,
+    },
+    textDisabled: {
+      color: themeColors.text.disabled,
+    },
+    textPrimary: {
+      color: themeColors.white,
+    },
+    textSecondary: {
+      color: themeColors.accent,
+    },
+  });
+};
