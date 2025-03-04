b8ad83d82cd1ecd6264295c11d7c9c0355def51e
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-05 03:14:59 +0800

    feat(provider): implement background actions

--- providers/WorkerProvider.tsx
+++ providers/WorkerProvider.tsx
@@ -1,8 +1,14 @@
-// Worker/WorkerProvider.tsx
-import React, { useMemo, useContext, createContext } from "react";
+import React, {
+  useMemo,
+  useContext,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+import { AppState } from "react-native";
+import BackgroundService from "react-native-background-actions";
 
 import { useUser } from "@/providers/UserProvider";
-
 import { useWorkerState } from "@/hooks/useWorkerState";
 
 import { useMutation, useQuery } from "convex/react";
@@ -13,10 +19,27 @@ import { Id } from "@/convex/_generated/dataModel";
 
 const WorkerContext = createContext<any>(undefined);
 
+const backgroundOptions = {
+  taskName: "Mining Worker",
+  taskTitle: "Worker is mining...",
+  taskDesc: "Mining coins in the background",
+  taskIcon: {
+    name: "ic_launcher",
+    type: "mipmap",
+  },
+  color: "#ff00ff",
+  linkingURI: "yourSchemeHere://mining",
+  parameters: {
+    delay: 1000,
+  },
+};
+
 export const WorkerProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const { user } = useUser();
+
+  const [fetchedWorker, setFetchedWorker] = useState<Worker | null>(null);
 
   const createWorker = useMutation(api.worker.createWorker);
   const deleteWorker = useMutation(api.worker.deleteWorker);
@@ -26,7 +49,17 @@ export const WorkerProvider: React.FC<{ children: React.ReactNode }> = ({
     api.worker.fetchWorker,
     user ? { userId: user?._id } : "skip",
   );
+
   const worker = ((workerQuery?.data || null) as Worker) || null;
+
+  const fetchWorker = async () => {
+    if (user) {
+      const workerData = await api.worker.fetchWorker({
+        userId: user._id as Id<"user">,
+      });
+      setFetchedWorker(workerData);
+    }
+  };
 
   const handleStartMining = async (userId: Id<"user">) => {
     await createWorker({ userId, rate: 1 });
@@ -36,10 +69,12 @@ export const WorkerProvider: React.FC<{ children: React.ReactNode }> = ({
     await deleteWorker({ userId });
   };
 
-  const handleUpdateBalance = async (balance: number) => {
+  const handleUpdateBalance = async (earnedAmount: number) => {
+    const updatedBalance = user.balance + earnedAmount;
+
     await updateUser({
-      userId: user?._id as Id<"user">,
-      updates: { balance },
+      userId: user._id as Id<"user">,
+      updates: { balance: updatedBalance },
     });
   };
 
@@ -48,6 +83,43 @@ export const WorkerProvider: React.FC<{ children: React.ReactNode }> = ({
     handleStopMining,
     handleUpdateBalance,
   );
+
+  useEffect(() => {
+    const startBackgroundService = async () => {
+      try {
+        const backgroundTask = async (taskData?: { delay: number }) => {
+          const delay = taskData?.delay ?? 1000;
+          while (BackgroundService.isRunning()) {
+            await fetchWorker();
+            await new Promise((resolve) => setTimeout(resolve, delay));
+          }
+        };
+
+        await BackgroundService.start(backgroundTask, backgroundOptions);
+      } catch (error) {
+        console.error("Error starting background service:", error);
+      }
+    };
+
+    const handleAppStateChange = (nextAppState: string) => {
+      console.log(nextAppState);
+      if (nextAppState === "background" || nextAppState === "inactive") {
+        startBackgroundService();
+      } else if (nextAppState === "active") {
+        BackgroundService.stop();
+      }
+    };
+
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange,
+    );
+
+    return () => {
+      subscription.remove();
+      BackgroundService.stop();
+    };
+  }, [fetchedWorker]);
 
   const contextValue = useMemo(
     () => ({
