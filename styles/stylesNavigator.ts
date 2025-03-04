17044d656c90f458cf5d2bc99de68822c1b8229d
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 23:59:16 +0800

    feat(styles): create navigator styles

---
+++ styles/stylesNavigator.ts
@@ -0,0 +1,31 @@
+// styles/Navigator.ts
+import { colors } from "@/styles/colors";
+
+import { StyleSheet } from "react-native";
+
+import { Theme } from "@/types/settings";
+
+export const createTabsNavigator = (theme: Theme) => {
+  const themeColors = theme === "Dark" ? colors.dark : colors.light;
+
+  return StyleSheet.create({
+    button: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    buttonCircle: {
+      bottom: 25,
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      backgroundColor: themeColors.view.secondary,
+    },
+    shadow: {
+      shadowColor: "#000000",
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.3,
+      shadowRadius: 3,
+      elevation: 4,
+    },
+  });
+};
