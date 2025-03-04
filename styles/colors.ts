652903f6e4d84f9658832eb6a3a24966e86e4996
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 23:07:19 +0800

    styles(colors): add colors

--- styles/colors.ts
+++ styles/colors.ts
@@ -5,21 +5,29 @@ export const colors = {
       primary: "#25242B",
       secondary: "#17171B",
       tertiary: "#35343B",
+      disabled: "gray",
     },
     text: {
       primary: "#FFFFFF",
+      disabled: "#cccccc",
     },
     accent: "#20AB7D",
+    white: "#FFFFFF",
+    negative: "#FF0000",
   },
   dark: {
     view: {
       primary: "#25242B",
       secondary: "#17171B",
       tertiary: "#35343B",
+      disabled: "gray",
     },
     text: {
       primary: "#FFFFFF",
+      disabled: "#cccccc",
     },
     accent: "#20AB7D",
+    white: "#FFFFFF",
+    negative: "#FF0000",
   },
 };
