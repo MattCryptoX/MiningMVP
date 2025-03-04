c8425c112067aa36f66aa42d4cdc26e4ef02c3ed
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-04 20:27:03 +0800

    feat(route): add gesture handler

--- app/navigator.tsx
+++ app/navigator.tsx
@@ -5,15 +5,18 @@ import { UserProvider } from "@/providers/UserProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function MainNavigator() {
   return (
     <UserProvider>
       <ThemeProvider>
-        <Stack>
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
+        <GestureHandlerRootView>
+          <Stack>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </GestureHandlerRootView>
       </ThemeProvider>
     </UserProvider>
   );
