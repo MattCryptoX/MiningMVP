b1c9b21dde9def8f8721e3d3b82a6a6ad4d75695
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-05 23:31:44 +0800

    feat(provider): init referral provider

---
+++ providers/ReferralProvider.tsx
@@ -0,0 +1,34 @@
+// Referral/ReferralProvider.tsx
+import React, {
+  useRef,
+  useMemo,
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+} from "react";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+const ReferralContext = createContext<any>(undefined);
+
+export const ReferralProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const contextValue = useMemo(() => ({}), []);
+
+  return (
+    <ReferralContext.Provider value={contextValue}>
+      {children}
+    </ReferralContext.Provider>
+  );
+};
+
+export const useReferral = () => {
+  const context = useContext(ReferralContext);
+  if (!context) {
+    throw new Error("useReferral must be used within a ReferralProvider");
+  }
+  return context;
+};
