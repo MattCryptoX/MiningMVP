4e8b293889d0d2a1c4c0275d9cb8845b24ab2ebb
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-05 23:32:55 +0800

    feat(route): wrap ReferralProvider

--- app/navigator.tsx
+++ app/navigator.tsx
@@ -8,6 +8,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { UserProvider } from "@/providers/UserProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { WorkerProvider } from "@/providers/WorkerProvider";
+import { ReferralProvider } from "@/providers/ReferralProvider";
 
 export default function MainNavigator() {
   return (
@@ -15,18 +16,20 @@ export default function MainNavigator() {
       <NotifierWrapper>
         <UserProvider>
           <ThemeProvider>
-            <WorkerProvider>
-              <Stack>
-                <Stack.Screen
-                  name={"(auth)"}
-                  options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                  name={"(tabs)"}
-                  options={{ headerShown: false }}
-                />
-              </Stack>
-            </WorkerProvider>
+            <ReferralProvider>
+              <WorkerProvider>
+                <Stack>
+                  <Stack.Screen
+                    name={"(auth)"}
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name={"(tabs)"}
+                    options={{ headerShown: false }}
+                  />
+                </Stack>
+              </WorkerProvider>
+            </ReferralProvider>
           </ThemeProvider>
         </UserProvider>
       </NotifierWrapper>
