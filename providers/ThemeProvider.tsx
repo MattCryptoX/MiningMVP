494b51c36674559229053d5e1f25f2de6d2652e4
Joshua Victoria <cryptoprojectemail4@gmail.com>
2025-03-07 00:01:18 +0800

    feat(provider): init theme provider

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,21 +1,46 @@
-// Theme/ThemeProvider.tsx
-import React, {
-  useMemo,
-  useState,
-  useEffect,
-  useContext,
-  createContext,
-} from "react";
+import React, { useReducer, useEffect, useContext, createContext } from "react";
 import { useColorScheme } from "react-native";
-
 import { useUser } from "@/providers/UserProvider";
-
+import { getLocales } from "expo-localization";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 
+import en from "@/language/en.json";
+
 import { Theme, UserSettings } from "@/types/settings";
 
+const translationsMap: Record<string, Record<string, any>> = {
+  en,
+};
+
+const deviceLanguage = getLocales()[0]?.languageCode || "en";
+
 const ThemeContext = createContext<any>(undefined);
+
+const initialState = {
+  theme: "Dark" as Theme,
+  language: deviceLanguage,
+  translations: translationsMap[deviceLanguage],
+};
+
+type Action =
+  | { type: "SET_THEME"; payload: Theme }
+  | { type: "SET_LANGUAGE"; payload: string };
+
+const themeReducer = (state: typeof initialState, action: Action) => {
+  switch (action.type) {
+    case "SET_THEME":
+      return { ...state, theme: action.payload };
+    case "SET_LANGUAGE":
+      return {
+        ...state,
+        language: action.payload,
+        translations: translationsMap[action.payload] || translationsMap["en"],
+      };
+    default:
+      return state;
+  }
+};
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -29,20 +54,27 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   );
   const settings = ((settingsQuery?.data || null) as UserSettings) || null;
 
-  const [theme, setTheme] = useState<Theme>("Dark");
+  const [state, dispatch] = useReducer(themeReducer, initialState);
 
   useEffect(() => {
     if (settings?.theme === "System") {
-      setTheme(colorScheme === "dark" ? "Dark" : "Light");
+      dispatch({
+        type: "SET_THEME",
+        payload: colorScheme === "dark" ? "Dark" : "Light",
+      });
     } else if (settings?.theme) {
-      setTheme(settings.theme);
+      dispatch({ type: "SET_THEME", payload: settings.theme });
+    }
+
+    if (settings?.language && translationsMap[settings.language]) {
+      dispatch({ type: "SET_LANGUAGE", payload: settings.language });
+    } else {
+      dispatch({ type: "SET_LANGUAGE", payload: deviceLanguage });
     }
   }, [settings, colorScheme]);
 
-  const contextValue = useMemo(() => ({ theme, setTheme }), [theme]);
-
   return (
-    <ThemeContext.Provider value={contextValue}>
+    <ThemeContext.Provider value={{ ...state, dispatch }}>
       {children}
     </ThemeContext.Provider>
   );
